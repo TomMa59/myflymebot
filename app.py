1660dcc8111790b7e6fd56b087ce6221f4f84f29
Thomas Maniez <th.maniez@gmail.com>
2022-03-14 10:25:27 +0100

    app.py update

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ DIALOG = MainDialog(RECOGNIZER, BOOKING_DIALOG, telemetry_client=TELEMETRY_CLIEN
 BOT = DialogAndWelcomeBot(CONVERSATION_STATE, USER_STATE, DIALOG, TELEMETRY_CLIENT)
 
 # Code for enabling activity and personal information logging.
-TELEMETRY_LOGGER_MIDDLEWARE = TelemetryLoggerMiddleware(telemetry_client=TELEMETRY_CLIENT, log_personal_information=False)
+TELEMETRY_LOGGER_MIDDLEWARE = TelemetryLoggerMiddleware(telemetry_client=TELEMETRY_CLIENT, log_personal_information=True)
 ADAPTER.use(TELEMETRY_LOGGER_MIDDLEWARE)
 
 # Listen for incoming requests on /api/messages.
